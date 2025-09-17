b191cf7a4ba2213ac587ccc8e10ffb2096e4ed9b
Hena2788 <henokfikadu712@gmail.com>
2025-09-17 13:50:56 +0300

    adding routing

--- Client/src/Pages/Landing/Landing.jsx
+++ Client/src/Pages/Landing/Landing.jsx
@@ -1,12 +1,18 @@
 import CarouselEffect from "../../Component/Carousel/Carousel"
+import Category from "../../Component/Category/Category"
 import Header from "../../Component/Header/Header"
+import Layout from "../../Component/Layout/Layout"
+import Product from "../../Component/Product/Product"
 
 
 const Landing = () => {
   return (
     <>
-    <Header/>
+    <Layout>
     <CarouselEffect/>
+    <Category/>
+    <Product/>
+    </Layout>
     </>
   )
 }
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+   <React.StrictMode>
+
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+   
   </React.StrictMode>
 );
