3175db62e690ae84bcf1f71ea73b9629f9d8a92f
Hena2788 <henokfikadu712@gmail.com>
2025-09-18 16:50:28 +0300

    adding a pages for Auth, orders, payment, cart

---
+++ Client/src/Pages/Orders/Orders.jsx
@@ -0,0 +1,14 @@
+
+import LayOut from '../../Component/Layout/Layout'
+
+const Orders = () => {
+  return (
+    <LayOut>
+
+      <div>Orders</div>
+    </LayOut>
+    
+  )
+}
+
+export default Orders
---
+++ Client/src/Pages/Payment/Payment.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LayOut from '../../Component/Layout/Layout'
+
+const Payment = () => {
+  return (
+    <LayOut>
+      <div>Payment</div>
+    </LayOut>
+    
+  )
+}
+
+export default Payment
