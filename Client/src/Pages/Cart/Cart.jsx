4a03986aca599f0fd5c1478d47c51fbf336a285f
Hena2788 <henokfikadu712@gmail.com>
2025-09-18 16:51:30 +0300

    adding a cart page

---
+++ Client/src/Pages/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import LayOut from '../../Component/Layout/Layout'
+
+const Cart = () => {
+  return (
+    <LayOut>
+      <div>Cart</div>
+    </LayOut>
+    
+  )
+}
+
+export default Cart
