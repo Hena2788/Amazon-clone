e817960a52ce259ba0099b5d6bf8b84d940a56a4
Hena2788 <Henokfikadu713@gmail.com>
2025-09-18 20:42:48 +0300

    adding loading

--- Client/src/Component/Loader/Loader.jsx
+++ Client/src/Component/Loader/Loader.jsx
@@ -1,11 +1,12 @@
-import {ScaleLoader} from 'react-spinners'
+import { ClipLoader } from "react-spinners";
+import styles from "./Loader.module.css";
 
 const Loader = () => {
   return (
-    <div style={{display:'flex',alignItems:'center',justifyContent:'center',height:'50vh'}}>
-      <ScaleLoader color="#37d7b7" />
+    <div className={styles.loaderContainer}>
+      <ClipLoader color="#37d7b7" />
     </div>
   );
-}
+};
 
-export default Loader
+export default Loader;
